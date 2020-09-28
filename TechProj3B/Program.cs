9e1db596c1218e0804586b572422c280c1f7d575
JHopple76 <70179411+JHopple76@users.noreply.github.com>
2020-09-27 21:37:44 -0400

    Update Program.cs

--- TechProj3B/Program.cs
+++ TechProj3B/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// Name: T. Jarrett Hopple
+// Class: ISM 4300
+// Assignment: Tech Project 3B
+
+using System;
 
 namespace TechProj3B
 {
@@ -6,7 +10,20 @@ namespace TechProj3B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Manually populated data");
+
+            // manually populated array
+            int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+            foreach (var item in array)
+            {
+                // shows the array values
+                Console.WriteLine("Element value = " + item);
+            }
+
+            // end of program
+            Console.ReadKey(true);
         }
+
     }
 }
+
